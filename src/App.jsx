a98892ccb1dd5c5b390d922800cf0fbf5aa041dd
Shivam Maurya <shivammaurya011@gmail.com>
2025-08-02 11:09:51 +0530

    prod done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
-
+import config from './config/env'
 function App() {
   return (
     <div className='text-4xl font-semibold text-amber-300 text-center my-40'>
-      Welcome Shivam Production
+      Welcome Shivam {config.appName} {config.nodeEnv}
     </div>
   )
 }
